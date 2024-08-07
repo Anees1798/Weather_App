dfa94afac10b56ae6ab4762863bc2c7908c027c9
anees <aneestruct@gmail.com>
2024-08-07 13:51:13 +0530

    day 32 : add sky condition images and make it in our webapp also temperature plot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Add Title, Text_input , slider, selectbox, and subheader
 st.title("Weather Forecast for the Next Days")
 
 place = st.text_input("place: ")
@@ -12,10 +13,28 @@ option = st.selectbox("select data to view",
                       ("temperature", "sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+# get the temperature/sky data
+filtered_data = get_data(place, days)
 
-data = get_data(place, days, option)
+# create a temperature plot
+if place:
+    if option == "temperature":
+        temperature = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperature,
+                         labels={"x": "Dates", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t,
-        labels={"x": "Dates", "y": "Temperature (C)"})
+    if option == "sky":
+        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+        images ={"Clouds": "images/cloud.png", "Clear": "images/clear.png",
+                 "Rain": "images/snow.png", "Snow": "images/snow.png"}
+        sky_image = [images[condition] for condition in sky_condition]
+        st.image(sky_image, width=115)
 
-st.plotly_chart(figure)
+
+
+
+
+
+
